4053474104ff8623ba7f8052a96f7b1174fe4a7b
wawa1111 <vioumchalermpipat@gmail.com>
2025-12-09 15:01:20 +0700

    update4

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 # Gemini Configuration
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyBh2WrdSXTMW6Mrc0ZGz75sWOsgeP3GMa4")
-GEMINI_MODEL = "gemini-pro"
-
+#GEMINI_MODEL = "gemini-pro" # old model used
+GEMINI_MODEL = "gemini-1.5-flash-latest"
 # Supabase Configuration
 SUPABASE_URL = os.getenv("SUPABASE_URL", "")
 SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY", "")
